b3094b7b984de26e5b20272d66c94f84b0c64008
Wang, Zilong <zwang937@gatech.edu>
2020-08-03 04:32:38 -0400

    Update gitignore

---
+++ Post-Process.R
@@ -0,0 +1,26 @@
+closeAllConnections()
+list.of.packages <- c("ggplot2", "Rcpp", "grf", "caret", "mltools", "rpart", "minpack.lm", "doParallel", "rattle", "anytime","rlist")
+list.of.packages <- c(list.of.packages, "zoo", "dtw", "foreach", "evaluate","rlist","data.table")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#install.packages("RApiDatetime", repos="http://cran.rstudio.com/", dependencies=TRUE)
+
+#install.packages("grf", repos="http://cran.rstudio.com/", dependencies=TRUE)
+
+#install.packages("rattle", repos="http://cran.rstudio.com/", dependencies=TRUE)
+
+lapply(list.of.packages, require, character.only = TRUE)
+
+
+# Set Working Directory to File source directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+registerDoParallel(cores=detectCores())
+
+###################################################################################
+# FILE IMPUPTES MISSING COUNTIES FROM RESULTS WITH ORIGINAL COUNTY DATA SET
+###################################################################################
+
+
