8c93f38d861fd6e6fe53583c14da1f3d3a9177ba
Wang <zwang937@isye-hps0005.isye.gatech.edu>
2020-09-17 21:29:26 -0400

    Redo Confusion

---
+++ src/master.sh
@@ -0,0 +1,6 @@
+Rscript Preprocess_us-counties-states.R
+Rscript Specific_Cases.R
+Rscript Block_Prepare.R
+Rscript State_Forests.R
+Rscript Preprocess_us-counties-states_minus7.R
+Rscript Post-Process-Block-Forests.R
