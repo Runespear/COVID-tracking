ca8ff1a27284e1b6de013c355863eadaf0e9d6a7
Zhaowei <zshe0902@gmail.com>
2020-09-22 20:55:33 -0400

    update to 9/21/20 data

--- src/Post-Process-Block-Forests.R
+++ src/Post-Process-Block-Forests.R
@@ -175,6 +175,7 @@ for (cutoff in cutofflist){
     #test.df$Rt<-with(test.df,exp(tau.hat*mu-0.5*(tau.hat**2)*(sigma**2)))
     
     test.df[which(test.df$state == "Massachusetts"),"Predicted_Double_Days"]<- NA
+    test.df[which(test.df$state == "Texas"),"Predicted_Double_Days"]<- NA
     
     final.df<-merge(x=test.df,y=county_14data, by="fips", all.x=TRUE)
     
